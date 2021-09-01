acc0897db6e30c8fe049e090055e432b9575f676
Tohir Raihan <tohir.raihan@gmail.com>
2021-09-02 04:07:28 +0600

    search result count section added

--- js/custom.js
+++ js/custom.js
@@ -7,21 +7,21 @@ const loadBooks = (url) => {
 
 // display books ---------------------------------------------------------------
 const displayBooks = (books) => {
+    console.log(books);
     // get search result field and clear previous results
     const searchItemsField = document.getElementById('search-items');
     searchItemsField.textContent = '';
 
     // creating single item to display in search result
     books.forEach((book) => {
-        console.log(book);
         // setting image url
-        const imgUrl = book.cover_i? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : 'images/avatar_book-sm.png';
+        const imgUrl = book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : 'images/avatar_book-sm.png';
         // preparing the search item div
         const div = document.createElement('div');
         div.classList.add('col-md-3', 'search-item');
         div.innerHTML = `
             <div class="card h-100">
-                <img height="200" src="${imgUrl}" class="card-img-top" alt="...">
+                <img height="250" src="${imgUrl}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title fw-bold">${book.title}</h5>
                     <p class="lead"><strong>by </strong>${book.author_name ? book.author_name.join(', ') : 'Unknown'}</p>
@@ -32,6 +32,21 @@ const displayBooks = (books) => {
         `;
         searchItemsField.appendChild(div);
     });
+    displayResultCount(books.length);
+};
+
+// display total result count --------------------------------------------------
+const displayResultCount = (total) => {
+    const resultCountInput = document.getElementsByClassName('result-count');
+    for (const resultCount of resultCountInput) {
+        console.log(resultCount);
+        resultCount.textContent = '';
+        const p = document.createElement('p');
+        p.innerHTML = `
+            <p>Total <span class="h5 fw-bold">${total}</span> search results found</p>
+        `;
+        resultCount.appendChild(p);
+    }
 };
 
 // search event
@@ -39,6 +54,7 @@ document.getElementById('search-button').addEventListener('click', () => {
     // get search text
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
+    searchInput.value = '';
     // dynamic url
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
 
