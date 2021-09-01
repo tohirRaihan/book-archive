e05503cb431019f84a880b2af298c332deaa6669
Tohir Raihan <tohir.raihan@gmail.com>
2021-09-02 01:46:12 +0600

    displayBooks function declaration

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,35 @@
-const loadBooks = url => {
+// load (fetch + display) all books
+const loadBooks = (url) => {
     fetch(url)
-    .then((res) => res.json())
-    .then((data) => console.log(data.docs));
-}
+        .then((res) => res.json())
+        .then((data) => displayBooks(data.docs));
+};
 
+// display books
+const displayBooks = (books) => {
+    // get search resuld field and clear previous results
+    const searchItemsField = document.getElementById('search-items');
+    searchItemsField.textContent = '';
+
+    // creating single item to display in search result
+    books.forEach((book) => {
+        // console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col-md-3', 'search-item');
+        div.innerHTML = `
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title fw-bold">Card title</h5>
+                    <p class="lead"><strong>by </strong> Gary B. Shelly, Thomas J. Cashman, William J. Dorin, Jeffrey J. Quasney</p>
+                    <p class="mb-0"><small class="text-muted">- First published in 2000</small></p>
+                    <p><small class="text-muted">- Published by <em><strong>Computer Step</strong></em></small></p>
+                </div>
+            </div>
+        `;
+        searchItemsField.appendChild(div);
+    });
+};
 
 // search event
 document.getElementById('search-button').addEventListener('click', () => {
@@ -15,6 +41,4 @@ document.getElementById('search-button').addEventListener('click', () => {
 
     // load books
     loadBooks(url);
-
-
 });
