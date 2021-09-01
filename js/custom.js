6612c606a06d4b86f7af5438c30b7d10ea89db0f
Tohir Raihan <tohir.raihan@gmail.com>
2021-09-02 02:09:23 +0600

    add dynamic author name in displayBooks function

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
-// load (fetch + display) all books
+// load (fetch + display) all books --------------------------------------------
 const loadBooks = (url) => {
     fetch(url)
         .then((res) => res.json())
         .then((data) => displayBooks(data.docs));
 };
 
-// display books
+// display books ---------------------------------------------------------------
 const displayBooks = (books) => {
     // get search resuld field and clear previous results
     const searchItemsField = document.getElementById('search-items');
@@ -13,7 +13,7 @@ const displayBooks = (books) => {
 
     // creating single item to display in search result
     books.forEach((book) => {
-        // console.log(book);
+        // console.log(...book.author_name);
         const div = document.createElement('div');
         div.classList.add('col-md-3', 'search-item');
         div.innerHTML = `
@@ -21,7 +21,7 @@ const displayBooks = (books) => {
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title fw-bold">Card title</h5>
-                    <p class="lead"><strong>by </strong> Gary B. Shelly, Thomas J. Cashman, William J. Dorin, Jeffrey J. Quasney</p>
+                    <p class="lead"><strong>by </strong>${book.author_name ? book.author_name.join(', ') : 'Unknown'}</p>
                     <p class="mb-0"><small class="text-muted">- First published in 2000</small></p>
                     <p><small class="text-muted">- Published by <em><strong>Computer Step</strong></em></small></p>
                 </div>
