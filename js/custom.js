df0c203f75a19be098b4763beb3c9c639870f8c9
Tohir Raihan <tohir.raihan@gmail.com>
2021-09-02 01:08:17 +0600

    get search text and make dynamic url

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,11 @@
 console.log('Iam connected');
+
+// search event
+document.getElementById('search-button').addEventListener('click', () => {
+    // get search text
+    const searchInput = document.getElementById('search-input');
+    const searchText = searchInput.value;
+    // dynamic url
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+
+});
